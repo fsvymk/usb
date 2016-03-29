3b164cd7793e52f4881d6b736054396a7db6dd71
Mihail <fsvymk@gmail.com>
2016-03-29 16:18:04 +0300

    pieces of shit #2

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
+// g++ source.cpp `pkg-config --libs --cflags libusb-1.0`
+
+//b#include "/usr/include/libusb-1.0/libusb.h"
+
+#include "libusb.h"
+//#include <linux/usb/>
+
+#include <linux/module.h>
+#include <linux/usbdevice_fs.h>
+#include <linux/kernel.h>
+#include <linux/usb.h>
 
 int main(int argc, char *argv[])
 {
+
+    libusb_init(NULL);
+
     printf("Hello World!\n");
     return 0;
 }
